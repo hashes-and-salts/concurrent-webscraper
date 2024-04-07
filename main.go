8d1f80382d409f9dae194d3b09c18c0db54fcda6
hash <hashesandsalts000@gmail.com>
2024-04-07 23:24:36 +0545

    concurrent scraping done

--- main.go
+++ main.go
@@ -4,14 +4,47 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 )
 
 func main() {
-	// url to scrape
-	url := ""
+
+	urls := []string{
+		// sites to scrape
+	}
+
+	done := make(chan bool)
+
+	for i, url := range urls {
+		go scrape(fmt.Sprintf("%d.txt", i), url, done)
+	}
+
+	for range urls {
+		<-done
+	}
+
+	fmt.Println("Scraping complete ", len(urls), " files written")
+
+}
+
+func scrape(filename string, url string, isComplete chan<- bool) {
+	f, err := os.Create(filename)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		isComplete <- false
+		return
+	}
+	defer f.Close()
+
 	response, _ := http.Get(url)
 
 	output, _ := io.ReadAll(response.Body)
 
-	fmt.Println("", string(output))
+	// fmt.Println("", string(output))
+
+	// f.WriteString(string(output))
+	f.Write(output)
+
+	fmt.Println("scraping complete, ", filename, " written")
+	isComplete <- true
 }
